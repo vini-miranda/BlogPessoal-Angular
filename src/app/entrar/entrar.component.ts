015508434fa5293f71fbb2b8d23ab0186573ad62
vini-miranda <v-alves-miranda@live.com>
2021-02-23 17:55:01 -0300

    componente inicio sem consumo

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -23,18 +23,13 @@ export class EntrarComponent implements OnInit {
   }
 
   entrar(){
-    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) => {
       this.userLogin = resp
 
       environment.token = this.userLogin.token
       environment.nome = this.userLogin.nome
       environment.foto = this.userLogin.foto
       environment.id = this.userLogin.id
-
-      console.log( environment.token)
-      console.log( environment.nome)
-      console.log( environment.foto)
-      console.log( environment.id)
 
       this.router.navigate(['/inicio'])
     }, erro => {
